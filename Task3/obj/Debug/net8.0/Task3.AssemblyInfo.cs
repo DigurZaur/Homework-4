83f4d7fe75d26a50adbe1c7625c4a7ee86aa626c
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-09 15:09:53 +0300

    Complete Task3

--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfa2e5d571574b0ef520148f707b58590ad8647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c731931419cbeb1a3fca965503fbbf480b54bd34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
